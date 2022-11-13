8a1cf4f741d54c40ba3579fbecba8da10e634ef4
George <georgematheuslima@outlook.com>
2022-11-13 11:34:56 -0300

    add alembic/env and do some changes at db_conf

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,9 @@ from sqlalchemy import pool
 from alembic import context
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+load_dotenv(os.path.join(BASE_DIR, ".env"))
+
+config.set_main_option("sqlalchemy.url", os.environ["DATABASE_URL"])
 
 config = context.config
 
@@ -15,7 +18,9 @@ if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 
-target_metadata = None
+import models
+
+target_metadata = models.Base.metadata
 
 
 def run_migrations_offline() -> None:
